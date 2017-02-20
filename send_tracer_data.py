f4c6f403b5f68b8368ed6e20608beb89b2781291
Martin Šenkeřík <martinsenkerik@gmail.com>
2017-02-20 20:59:46 +0000

    Reinit the Tracer module in case of failure. Precalculate battery percentage for the emonCMS.

--- send_tracer_data.py
+++ send_tracer_data.py
@@ -10,10 +10,13 @@ from tracer import Tracer, TracerSerial, QueryCommand
 with open(sys.argv[1], 'r') as keyfile:
     EMON_CMS_KEY = keyfile.read().strip()
 
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
-tracer = Tracer(0x16)
-tracer_serial = TracerSerial(tracer, port)
-query = QueryCommand()
+
+def init():
+   global tracer_serial, query
+   port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
+   tracer = Tracer(0x16)
+   tracer_serial = TracerSerial(tracer, port)
+   query = QueryCommand()
 
 def clean_data(data):
    del(data["data"])
@@ -22,6 +25,14 @@ def clean_data(data):
          data[key] = 1 if value else 0
    return data
 
+def add_batt_percentage(data):
+   full_v = data["batt_full_voltage"]
+   empty_v = data["batt_overdischarge_voltage"]
+   bat_v = data["batt_voltage"]
+   data["batt_percentage"] = (bat_v - empty_v) / (full_v - empty_v) * 100
+
+
+init()
 
 while True:
    try:
@@ -29,9 +40,11 @@ while True:
       tracer_data = tracer_serial.receive_result()
 
       load = clean_data(tracer_data.__dict__)
+      add_batt_percentage(load)
    
       print get("https://emoncms.org/input/post.json?node=1&apikey=%s&json=%s" % (EMON_CMS_KEY, json.dumps(load))).text
    except Exception as e:
       print >> sys.stderr, e
+      init()
    
    sleep(10)
